4c99c5ab9f20b21ce00b7c2dd9728d286e266187
gv-prashanth <gvprashanth.android@gmail.com>
2018-11-06 21:40:05 +0530

    Initial commit

---
+++ src/main/java/com/vadrin/helloworldhtml/HelloWorldHtmlApplication.java
@@ -0,0 +1,12 @@
+package com.vadrin.helloworldhtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldHtmlApplication.class, args);
+	}
+}
